d8b095d83e8a9fef2b1773285de5753804623f74
abburigithubaln <141417346+abburigithubaln@users.noreply.github.com>
2023-08-05 17:29:25 +0530

    first commit

---
+++ src/components/Formtable.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "../App.css"
+import { MdCancel } from 'react-icons/md'
+
+const Formtable = ({handleSubmit,handleOnChange,handleClose,rest}) => {
+  return (
+    <div className="addContainer">
+         <form 
+           onSubmit={handleSubmit}>
+           <div className="close-btn" onClick={handleClose}><MdCancel/></div>
+            <label htmlFor="name">Name :</label>
+            <input type="text" id="name" name="name" onChange={handleOnChange} value={rest.name}/>
+
+            <label htmlFor="email">Email :</label>
+            <input type="email" id="email" name="email" onChange={handleOnChange} value={rest.email}/>
+
+            <label htmlFor="mobile">Mobile :</label>
+            <input type="number" id="mobile" name="mobile" onChange={handleOnChange} value={rest.mobile}/>
+
+           <button className="btn">Submit</button>     
+          </form>
+      </div>
+  )
+}
+
+export default Formtable
